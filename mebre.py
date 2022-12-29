9f542d4fd5c02a528d4ce834af80b5c9cf045bea
kidisttesfaye <mebratmemihru@gmail.com>
2022-12-29 16:24:17 +0300

    By mebrat

---
+++ mebre.py
@@ -0,0 +1,11 @@
+"""print("Enter your name")
+c=input()
+def demo(names):
+    print("Hello "+names)
+demo(c)"""    
+print("Enter the first number")
+a=int(input())
+print("Enter the second number")
+b=int(input())
+sum= a+b
+print("sum =",sum)   
