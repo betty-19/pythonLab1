a1abb3ee456c0c42b2e0ee741e324eb7c1fc1dba
Betelhem Sintayehu <betelhemsintayehu10@gmail.com>
2022-12-29 13:11:45 +0000

    edited by yab

--- yab.py
+++ yab.py
@@ -3,5 +3,24 @@ def sum():
 sum()
 x,y,z="kaleb","hailab","bety"
 print(x)
-x=y=z="yabu"
-print(z)
+s=t=u="yabu"
+print(u)
+a=2
+b=2.5
+print(type(b))
+print(len(u))
+name=input("Enter your name ")
+print(name)
+age=input("enter your age ")
+print(age)
+n1=input("Enter number1")
+print(name)
+n2=input("Enter number2")
+print(name)
+sum=int(n1)*int(n2)
+print("The bezat of n1 and n2 " + sum)
+name="yab"
+age=8
+cgpa=9.564336
+print("my name is {:s} i am {:d} my cgpa is {:f}".format(name,age,cgpa))
+
