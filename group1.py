be6c5ba9817cf6194cf5e9ad1cb3132d88ab0b7b
Betelhem Sintayehu <betelhemsintayehu10@gmail.com>
2023-01-04 23:30:29 +0300

    i'm bety

--- group1.py
+++ group1.py
@@ -1,4 +1,29 @@
+"""
 is_cs = True
 is_group1 = True
 if is_cs and is_group1:
     print("YOU ARE GROUP1 CS STUDENT")
+"""
+def say_hello():
+ print("Hello there!")
+print(say_hello())
+def say_hello(): print("Hello theree!")
+print(say_hello())
+n = 'hello'
+print(len(n))
+print(ord("h"))
+print(type(n))
+print ("I am 9 \" tall")
+print(n.isdecimal()) 
+print(n.isalnum())
+age =7 
+ticket_price = 20 if int(age) >= 18 else 5
+print(ticket_price)
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+for x,num in enumerate(num_list):
+ if num == 36:
+    print('Number found at ', x)
+
+
+ 
+
